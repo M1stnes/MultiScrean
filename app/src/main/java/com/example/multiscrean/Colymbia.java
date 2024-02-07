e11637c1e0720057a89aba97974027f32e2db1d7
Efimov <efimov@iMac-Administrator.local>
2024-02-07 13:40:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/multiscrean/Colymbia.java
@@ -0,0 +1,14 @@
+package com.example.multiscrean;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Colymbia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colymbia);
+    }
+}
---
+++ app/src/main/java/com/example/multiscrean/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.multiscrean;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity {
+
+    String[] things = {"Аргентина","Бразилия","Чили","Колумбия","Уругвай"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, things);
+        setListAdapter(adapter);
+
+        AdapterView.OnItemClickListener itemListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        Intent intent1 = new Intent(MainActivity.this, Argentina.class);
+                        startActivity(intent1);
+                        break;
+
+                    case 1:
+                        Intent intent2 = new Intent(MainActivity.this, Brazil.class);
+                        startActivity(intent2);
+                        break;
+
+                    case 2:
+                        Intent intent3 = new Intent(MainActivity.this, Chyly.class);
+                        startActivity(intent3);
+                        break;
+
+                    case 3:
+                        Intent intent4 = new Intent(MainActivity.this, Colymbia.class);
+                        startActivity(intent4);
+                        break;
+
+                    case 4:
+                        Intent intent5 = new Intent(MainActivity.this, yrygvai.class);
+                        startActivity(intent5);
+                        break;
+                }
+                Toast.makeText(getApplicationContext(), "Вы выбрали " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+            }
+        };
+        getListView().setOnItemClickListener(itemListener);
+    }
+}
